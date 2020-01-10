66bf188f58af2d4bbb86d8ee10111b4f82b75f84
Naresh <naresh.mehta8800@gmail.com>
2020-01-10 17:02:10 +0530

    working on spinner

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import {
     MatFormFieldModule, MatInputModule, MatButtonModule,
-    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule
 } from '@angular/material';
 @NgModule({
-    imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatTabsModule, MatSelectModule, MatCardModule,
+    imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatTabsModule, MatProgressSpinnerModule, MatSelectModule, MatCardModule,
         MatIconModule, MatListModule],
-    exports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatTabsModule, MatSelectModule, MatCardModule, MatIconModule, MatListModule]
+    exports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatProgressSpinnerModule, MatToolbarModule, MatTabsModule, MatSelectModule, MatCardModule, MatIconModule, MatListModule]
 })
 export class MaterialModule { }
